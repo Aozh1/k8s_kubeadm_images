f16b00b3ab9182d34b3bbeae25bd70bbff673366
Aozhi <31535219+Aozh1@users.noreply.github.com>
2019-04-12 17:09:52 +0800

    Update k8spull.sh

--- k8spull.sh
+++ k8spull.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 images=(
-    kube-apiserver-amd64:v1.14.1
-    kube-controller-manager-amd64:v1.14.1
-    kube-scheduler-amd64:v1.14.1
-    kube-proxy-amd64:v1.14.1
+    kube-apiserver-amd64:
+    kube-controller-manager-amd64:
+    kube-scheduler-amd64:
+    kube-proxy-amd64:=
     pause:3.1
-    etcd-amd64:3.3.10
-    coredns:1.3.1
+    etcd-amd64:
+    coredns:
 
-    pause-amd64:3.1
+    pause-amd64:
 
-    kubernetes-dashboard-amd64:v1.10.0
-    heapster-amd64:v1.5.4
-    heapster-grafana-amd64:v5.0.4
-    heapster-influxdb-amd64:v1.5.2
+    kubernetes-dashboard-amd64:
+    heapster-amd64:
+    heapster-grafana-amd64:
+    heapster-influxdb-amd64:
 )
 
 for imageName in ${images[@]} ; do
